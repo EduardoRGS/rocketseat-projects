1607905af886b36ce021b0db83ef0ca8be0d1f7b
Eduardo Ribeiro <dudub9go@hotmail.com>
2023-07-26 09:37:14 -0300

    Criação do componente de comentario e do css dele

---
+++ ignite-feed/src/components/Comment.tsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comments.module.css'
+
+export function Comment()  {
+    return (
+    <div className={styles.comment}>
+        <img src='https://github.com/EduardoRGS.png' />
+        
+        <div className={styles.commentBox}>
+            <div className={styles.commentContent}>
+                <header>
+
+                    <div className={styles.authorAndTime}>
+                        <strong>Eduardo Ribeiro</strong>
+                        <time title='26 de Julho ás 09:05h' dateTime='2023-7-26 09:05:30'>Cerca de 1h atrás</time>
+                    </div>
+
+                    <button title='Deleter comentário'>
+                        <Trash size={24}></Trash>
+                    </button>
+
+                </header>
+
+                <p>Muito bom Devon, parabéns!! 👏👏 </p>
+            </div>
+
+            <footer>
+                <button>
+                    <ThumbsUp />
+                    Aplaudir <span>20</span>
+                </button>
+            </footer>
+
+        </div>
+    </div>
+    )
+}
