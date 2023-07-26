7a405ad7655abe60a373ffae329260675512e2fe
Eduardo Ribeiro <dudub9go@hotmail.com>
2023-07-26 09:56:55 -0300

    Utilização do comment avatar

--- ignite-feed/src/components/Comment.tsx
+++ ignite-feed/src/components/Comment.tsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comments.module.css'
+import { Avatar } from './Avatar'
 
 export function Comment()  {
     return (
     <div className={styles.comment}>
-        <img src='https://github.com/EduardoRGS.png' />
+        <Avatar src='https://github.com/EduardoRGS.png' />
         
         <div className={styles.commentBox}>
             <div className={styles.commentContent}>
--- ignite-feed/src/components/Sidebar.tsx
+++ ignite-feed/src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 import { PencilLine } from 'phosphor-react'
 
@@ -9,10 +10,7 @@ export function Sidebar() {
             src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=40"></img>
 
             <div className={styles.profile}>
-                <img
-                className={styles.avatar}
-                 src="https://github.com/EduardoRGS.png"></img>
-
+               <Avatar hasBorder src="https://github.com/EduardoRGS.png"/>
                 <strong>Eduardo Ribeiro</strong>
                 <span>FullStack Developer</span>
             </div>
