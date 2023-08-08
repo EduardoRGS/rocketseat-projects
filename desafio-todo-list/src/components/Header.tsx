40b2a87ff80f86d9e85e34880a5456a7ce4e74b2
Eduardo Ribeiro <dudub9go@hotmail.com>
2023-08-08 20:50:20 -0300

    Mudanças componente Header

--- desafio-todo-list/src/components/Header.tsx
+++ desafio-todo-list/src/components/Header.tsx
@@ -1,12 +1,10 @@
 import styles from './Header.module.css'
 import todoLogo from '../assets/todo.png'
-import rocketLogo from '../assets/rocket.png'
 
 export function Header () {
 
     return (
         <header className={styles.header} >
-            <img src={rocketLogo} />
              <img src={todoLogo} />
          </header>
     )
