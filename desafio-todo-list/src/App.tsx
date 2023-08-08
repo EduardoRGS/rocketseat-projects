5886bb80c42e464a4814a4b8554db14f48bd4fa0
Eduardo Ribeiro <dudub9go@hotmail.com>
2023-08-08 20:51:51 -0300

    Adicionando componentes no index app

--- desafio-todo-list/src/App.tsx
+++ desafio-todo-list/src/App.tsx
@@ -1,10 +1,20 @@
-import { Header } from './components/Header'
+import { useState } from "react";
+import { Header } from "./components/Header";
+import { TaskList } from "./components/TaskList";
+import { TaskSubmitForm, TaskTypes } from "./components/TaskSubmitForm";
+
+import styles from "./App.module.css";
 
 function App() {
+  const [tasks, setTasks] = useState<TaskTypes[]>([]);
 
   return (
     <div>
-      <Header></Header>
+     <Header />
+      <div className={styles.content}>
+        <TaskSubmitForm tasks={tasks} setTasks={setTasks} />
+        <TaskList tasks={tasks} setTasks={setTasks} />
+      </div>
     </div>
   )
 }
